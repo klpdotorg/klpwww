3229a6db350f785d58bc33501e7f524902fd4076
meghaklp <megha@klp.org.in>
2012-02-10 16:35:38 +0530

    corrected typo

--- importers/coord/update-circles.sh
+++ importers/coord/update-circles.sh
@@ -4,4 +4,4 @@ DBNAME=klp-coord
 OWNER=klp
 
 echo Loading data...
-psql -U ${OWNER} -d ${DBNAME} -f load/update-circle.sql && echo done.
+psql -U ${OWNER} -d ${DBNAME} -f load/update_circle.sql && echo done.

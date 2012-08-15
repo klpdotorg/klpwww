80bb6a0f23ca4c584642882124aa14d6e5d4f3f1
Megha Vishwanath <megha@dev.cloudmin.e2enetworks.net.in>
2012-08-15 11:39:24 +0530

    Updates for circles

--- importers/coord/klp-coord.sh
+++ importers/coord/klp-coord.sh
@@ -17,5 +17,5 @@ psql -U ${OWNER} -d ${DBNAME} -f ${DBNAME}.sql && echo done.
 
 echo Loading data...
 psql -U ${OWNER} -d ${DBNAME} -f load/load-klp-coord.sql && echo done.
-psql -U ${OWNER} -d ${DBNAME} -f load/update-inst-coord.sql && echo done.
-psql -U ${OWNER} -d ${DBNAME} -f load/update-circle.sql && echo done.
+psql -U ${OWNER} -d ${DBNAME} -f load/update_inst_coords.sql && echo done.
+psql -U ${OWNER} -d ${DBNAME} -f load/update_circle.sql && echo done.

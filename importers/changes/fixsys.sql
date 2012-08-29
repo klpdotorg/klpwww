9c21207b03556b9949093e563c5636fa37607508
Megha Vishwanath <megha@dev.cloudmin.e2enetworks.net.in>
2012-08-29 17:13:31 +0530

    deleting pic for 33097 against ticket 468

--- importers/changes/fixsys.sql
+++ importers/changes/fixsys.sql
@@ -2,3 +2,4 @@ update tb_sys_data set schoolid=32912 where id=2348;
 update tb_sys_data set schoolid=32895 where id=2365;
 update tb_sys_images set schoolid=32912 where sysid=2348;
 update tb_sys_images set schoolid=32895 where sysid=2365;
+delete from tb_sys_images where schoolid=33097 and sysid=2145;

477ef6cfd00a4ede50e72febeccbb7d6ac848871
Shivangi Desai <shivangi@dev.cloudmin.e2enetworks.net.in>
2012-09-22 11:48:16 +0530

    2011-2012 ang and english assesment and data

--- importers/klp-exp.py
+++ importers/klp-exp.py
@@ -9,7 +9,7 @@ import Utility.KLPDB
 connection = Utility.KLPDB.getConnection()
 cursor = connection.cursor()
 
-pidAyid={'1':90,'2':1,'3':2,'5':119,'6':119,'7':119,'8':119,'9':119,'10':119,'11':119,'12':119,'13':119,'14':101,'15':101,'18':101,'19':101}
+pidAyid={'1':90,'2':1,'3':2,'5':119,'6':119,'7':119,'8':119,'9':119,'10':119,'11':119,'12':119,'13':119,'14':101,'15':101,'18':101,'19':102,'23':102,'24':102,'25':102,'26':102,'27':102,'28':102,'29':102,'30':102,'31':102}
 
 
 queries=[{'tb_bhierarchy':'select distinct id,boundary_category from schools_boundary_category'},
@@ -22,10 +22,10 @@ queries=[{'tb_bhierarchy':'select distinct id,boundary_category from schools_bou
 {'tb_academic_year':'select * from schools_academic_year'},
 {'tb_student':'select * from schools_student'},
 {'tb_student_class':"select stusg.student_id,stusg.student_group_id,stusg.academic_id,stusg.active from schools_student_studentgrouprelation stusg,schools_studentgroup sg where stusg.student_group_id=sg.id and sg.group_type='Class'"},
-{'tb_programme':'select id,name,"startDate","endDate",programme_institution_category_id from schools_programme where id in (1,2,3,5,6,7,8,9,14,15,18,19)'},
-{'tb_assessment':'select ass.id,ass.name,ass.programme_id,ass."startDate",ass."endDate" from schools_assessment ass,schools_programme p where ass.programme_id=p.id and p.id in (1,2,3,5,6,7,8,9,14,15,18,19)'},
-{'tb_question':'select q.id,q.assessment_id,q.name,q."questionType",q."scoreMax",q."scoreMin",q.grade from schools_question q, schools_assessment ass,schools_programme p where q.assessment_id=ass.id and ass.programme_id=p.id and p.id in (1,2,3,5,6,7,8,9,14,15,18,19)'},
-{'tb_student_eval':'select se.question_id,se.student_id,se."answerScore",se."answerGrade" from schools_answer se,schools_question q, schools_assessment ass,schools_programme p where se.question_id=q.id and q.assessment_id=ass.id and ass.programme_id=p.id and p.id in (1,2,3,5,6,7,8,9,14,15,18,19)'},
+{'tb_programme':'select id,name,"startDate","endDate",programme_institution_category_id from schools_programme where id in (1,2,3,5,6,7,8,9,14,15,18,19,23,24,25,26,27,28,29,30,31)'},
+{'tb_assessment':'select ass.id,ass.name,ass.programme_id,ass."startDate",ass."endDate" from schools_assessment ass,schools_programme p where ass.programme_id=p.id and p.id in (1,2,3,5,6,7,8,9,14,15,18,19,23,24,25,26,27,28,29,30,31)'},
+{'tb_question':'select q.id,q.assessment_id,q.name,q."questionType",q."scoreMax",q."scoreMin",q.grade from schools_question q, schools_assessment ass,schools_programme p where q.assessment_id=ass.id and ass.programme_id=p.id and p.id in (1,2,3,5,6,7,8,9,14,15,18,19,23,24,25,26,27,28,29,30,31)'},
+{'tb_student_eval':'select se.question_id,se.object_id,se."answerScore",se."answerGrade" from schools_answer se,schools_question q, schools_assessment ass,schools_programme p where se.question_id=q.id and q.assessment_id=ass.id and ass.programme_id=p.id and p.id in (1,2,3,5,6,7,8,9,14,15,18,19,23,24,25,26,27,28,29,30,31)'},
 {'tb_teacher': 'select t.id,lower(trim(t."firstName")),lower(trim(t."middleName")),lower(trim(t."lastName")),t.gender,t.active,mt.name,t.doj,type."staffType" from schools_staff t left outer join schools_moi_type mt on (t.mt_id=mt.id) left outer join schools_staff_type type on (t.staff_type_id=type.id)'},
 {'tb_teacher_qual':'select sq.staff_id,qual.qualification from schools_staff_qualification sq,schools_staff_qualifications qual where sq.staff_qualifications_id=qual.id'},
 {'tb_teacher_class': 'select staff_id,student_group_id,academic_id,active from schools_staff_studentgrouprelation'}]

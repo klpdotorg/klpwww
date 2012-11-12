5930e73bf259ae694f24f11cac4a8fe92a5a552b
Megha Vishwanath <megha@dev.cloudmin.e2enetworks.net.in>
2012-11-12 13:29:07 +0530

    Fixing ticket 516 on locations of schools

--- importers/coord/fixes.sql
+++ importers/coord/fixes.sql
@@ -1,1 +1,2 @@
 update inst_coord  set coord = GeomFromText('POINT(77.76378 12.9978)') where instid=33332;
+update inst_coord  set coord = GeomFromText('POINT(77.59167 12.86575)') where instid=33095;

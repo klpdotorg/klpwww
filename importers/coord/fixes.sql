4e966847e727ac2f91c01da2636ed1682d95879d
Megha Vishwanath <megha@dev.cloudmin.e2enetworks.net.in>
2012-12-24 12:54:16 +0530

    Preventing marker overlap, moved marker by 0.0001 point

--- importers/coord/fixes.sql
+++ importers/coord/fixes.sql
@@ -1,3 +1,3 @@
 update inst_coord  set coord = GeomFromText('POINT(77.76378 12.9978)') where instid=33332;
 update inst_coord  set coord = GeomFromText('POINT(77.59167 12.86575)') where instid=33095;
-update inst_coord  set coord = GeomFromText('POINT(77.6161 12.96398)') where instid=33137;
+update inst_coord  set coord = GeomFromText('POINT(77.6162 12.96399)') where instid=33137;

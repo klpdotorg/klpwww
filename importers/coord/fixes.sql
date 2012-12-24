a27957b494e31b169e489f0801ef8ebbeeebeedf
Megha Vishwanath <megha@dev.cloudmin.e2enetworks.net.in>
2012-12-24 12:20:48 +0530

    Changes as for trac ticket 532 - 33127  and 33137 are both on linden st

--- importers/coord/fixes.sql
+++ importers/coord/fixes.sql
@@ -1,2 +1,3 @@
 update inst_coord  set coord = GeomFromText('POINT(77.76378 12.9978)') where instid=33332;
 update inst_coord  set coord = GeomFromText('POINT(77.59167 12.86575)') where instid=33095;
+update inst_coord  set coord = GeomFromText('POINT(77.6161 12.96398)') where instid=33137;

02c6fdf1b6186b66708c8c401f3ac1e0def0d766
Sajjad Anwar <sajjadkm@gmail.com>
2013-02-07 11:51:30 +0530

    Bounds for the map

--- js/maps.js
+++ js/maps.js
@@ -17,11 +17,15 @@ function getURLParameter(name) {
     );
 }
 
+var southWest = new L.LatLng(11.57, 73.87),
+    northEast = new L.LatLng(18.45, 78.57),
+    bounds = new L.LatLngBounds(southWest, northEast);
+
 var regionParameter = getURLParameter('region');
 var region = (regionParameter === 'undefined') ? '' : regionParameter;
 var bangalore = L.latLng([12.9719,77.5937]);
 
-var map = L.map('map', {zoomControl: false, attributionControl: false}).setView(bangalore, 12);
+var map = L.map('map', {zoomControl: false, attributionControl: false, maxBounds: bounds}).setView(bangalore, 12);
 var mapquestUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
 subDomains = ['otile1','otile2','otile3','otile4'];
 var mapquest = new L.TileLayer(mapquestUrl, {maxZoom: 18, subdomains: subDomains});
